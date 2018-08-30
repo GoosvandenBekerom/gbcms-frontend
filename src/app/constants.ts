8cdc4a859b955c3bbad7f5654f199c848b3d7ef5
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-08-30 18:17:26 +0200

    Created file to hold all constant values

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+export const PROTOCOL = 'http';
+export const HOST = 'localhost:8080';
+export const BASE_URL = `${PROTOCOL}://${HOST}`;
+export const API_BASE_URL = `${PROTOCOL}://${HOST}`;
+
+export const LOCALSTORAGE_TOKEN_KEY = 'auth_token';
+export const AUTHORIZATION_HEADER_KEY = 'Authorization';
+export const AUTHORIZATION_TOKEN_PREFIX = 'Bearer ';
